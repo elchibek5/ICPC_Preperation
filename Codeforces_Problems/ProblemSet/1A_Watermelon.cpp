62897bff82991811818128710ccee06f45b76d99
elchibekdastanov <elchibekdastanov@gmail.com>
2026-03-30 13:00:34 -0500

    .

--- Codeforces_Problems/ProblemSet/1A_Watermelon.cpp
+++ Codeforces_Problems/ProblemSet/1A_Watermelon.cpp
@@ -3,14 +3,12 @@
 using namespace std;
 
 string checkParity(int number) {
-    string checkParity(int number) {
     // It must be even AND greater than 2
     if (number > 2 && number % 2 == 0) {
         return "YES";
     } else {
         return "NO";
     }
-}
 }
 
 int main() {
