77d108e3c285239d8296d4036bc6f0bb7f22ddbc
elchibekdastanov <elchibekdastanov@gmail.com>
2026-04-04 16:59:56 -0500

    Codeforces

--- Codeforces_Problems/ProblemSet/25A.cpp
+++ Codeforces_Problems/ProblemSet/25A.cpp
@@ -7,28 +7,24 @@ int main() {
     cin >> n;
     vector<int> a(n);
 
-    int odd = 0;
-    int even = 0;
+    int odd = 0, even = 0;
+    int last_odd = 0, last_even = 0;
 
     for (int i = 0; i < n; i++) {
         cin >> a[i];
-        if (a[i] % 2 == 0) even++;
-        else odd++;
+        if (a[i] % 2 == 0) {
+            even++;
+            last_even = i + 1;
+        }
+        else {
+            odd++;
+            last_odd = i + 1;
+        }
     }
 
-    if (even == 1) {
-        for (int i = 0; i < n; i++) {
-            if (a[i] % 2 == 0) {
-                cout << i + 1;
-            }
-        }
+    if (odd == 1) {
+        cout <<  last_odd;
     } else {
-        for (int i = 0; i < n; i++) {
-            if (a[i] % 2 != 0) {
-                cout << i + 1;
-            }
-        }
+        cout << last_even;
     }
-
-    return 0;
 }
