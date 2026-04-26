b2fcbf723bc162a3df96f3cea1301080a707cede
Elchibek Dastanov <elchibekdastanov@gmail.com>
2026-04-25 20:00:33 -0500

    Add second solution for Insertion Sort List

--- LeetCode_Problems/Java/medium/147. Insertion Sort List.java
+++ LeetCode_Problems/Java/medium/147. Insertion Sort List.java
@@ -32,3 +32,22 @@ class Solution {
 
     }
 }
+
+
+// SECOND SOLUTION
+
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        for (ListNode cur = head; cur != null; cur = cur.next) {
+            for (ListNode j = head; j != cur; j = j.next) {
+                if (j.val > cur.val) {
+                    int t = j.val;
+                    j.val = cur.val;
+                    cur.val = t;
+                }
+            }
+        }
+        return head;
+
+    }
+}
