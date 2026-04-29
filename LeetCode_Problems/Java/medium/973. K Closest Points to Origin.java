dfbddd3d42c3aced32fac1fdb554a5622761556f
Elchibek Dastanov <elchibekdastanov@gmail.com>
2026-04-29 18:10:13 -0500

    Refactor kClosest method to use quickSelect

--- LeetCode_Problems/Java/medium/973. K Closest Points to Origin.java
+++ LeetCode_Problems/Java/medium/973. K Closest Points to Origin.java
@@ -1,24 +1,51 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> {
-            int distA = a[0] * a[0] + a[1] * a[1];
-            int distB = b[0] * b[0] + b[1] * b[1];
-            return Integer.compare(distB, distA);
-        });
+        int n = points.length;
 
-        for (int i = 0; i < points.length; i++) {
-            maxHeap.add(points[i]);
+        int[][] dist_index = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            dist_index[i][0] = points[i][0] * points[i][0] + points[i][1] * points[i][1];
+            dist_index[i][1] = i;
+        }
 
-            if (maxHeap.size() > k) {
-                maxHeap.poll();
+        quickSelect(dist_index, 0, n - 1, k - 1);
+
+        int[][] answer = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            answer[i] = points[dist_index[i][1]];
+        }
+        return answer;
+    }
+
+    private static void quickSelect(int[][] arr, int left, int right, int k) {
+        Random rand = new Random();
+        int randIndex = rand.nextInt(left, right + 1);
+        swap(arr, left, randIndex);
+
+        int pivot = arr[left][0];
+        int storeIndex = left + 1;
+        for (int i = left + 1; i <= right; i++) {
+            if (arr[i][0] < pivot) {
+                swap(arr, i, storeIndex);
+                storeIndex++;
             }
         }
 
-        int[][] result = new int[k][2];
-        for (int i = 0; i < k; i++) {
-            result[i] = maxHeap.poll();
-        }
-        
-        return result;
+        swap(arr, left, storeIndex - 1);
+
+        int l = storeIndex - 2;
+        int r = storeIndex;
+        while (l >= left && arr[l][0] == pivot) l--;
+        while (r <= right && arr[r][0] == pivot) r++;
+
+        if (l + 1 <= k && k <= r - 1) return;
+        if (k <= l) quickSelect(arr, left, l, k);
+        else quickSelect(arr, r, right, k);
+    }
+
+    private static void swap(int[][] arr, int i, int j) {
+        int[] tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp; 
     }
 }
