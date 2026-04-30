29e9487ec727151bd6138ac28a6fc2e7bd97af9f
Elchibek Dastanov <elchibekdastanov@gmail.com>
2026-04-30 13:42:02 -0500

    Refactor maxProfit method for stock trading
    Refactor maxProfit method to use a greedy approach for calculating profit by buying and selling on appropriate days.

--- LeetCode_Problems/Java/medium/122. Best Time to Buy and Sell Stock II.java
+++ LeetCode_Problems/Java/medium/122. Best Time to Buy and Sell Stock II.java
@@ -1,10 +1,23 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        // Greedy Approach
+        int days = prices.length;
         int profit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                profit += prices[i] - prices[i - 1];
+        int buy_day = 0;
+        int sell_day = 0;
+
+        while (buy_day < days && sell_day < days) {
+            while (buy_day < days - 1 && prices[buy_day] >= prices[buy_day + 1]) {
+                buy_day++;
             }
+
+            sell_day = buy_day + 1;
+            while (sell_day < days - 1 && prices[sell_day] <= prices[sell_day + 1]) {
+                sell_day++;
+            }
+
+            profit += prices[sell_day] - prices[buy_day];
+            buy_day = sell_day + 1;
         }
 
         return profit;
