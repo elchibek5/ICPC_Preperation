70f5bea83a2fa1b81409b34b4cb89cc9628d8a0e
Elchibek Dastanov <elchibekdastanov@gmail.com>
2026-05-14 23:49:34 -0500

    Refactor findMin method for clarity and efficiency

--- LeetCode_Problems/Java/medium/0153. Find Minimum in Rotated Sorted Array.java
+++ LeetCode_Problems/Java/medium/0153. Find Minimum in Rotated Sorted Array.java
@@ -1,27 +1,20 @@
 class Solution {
     public int findMin(int[] nums) {
         int n = nums.length;
+        int l = 0;
+        int r = n - 1;
 
-        if (nums == null || n == 0) return 0;
+        int mid = (r + l) / 2;
 
-        if (n == 1) return nums[0];
-
-        if (nums[0] < nums[n - 1]) return nums[0];
-
-        int left = 0;
-        int right = n - 1;
-        //[4,5,6,7,0,1,2]
-
-        while (left <= right) {
-            int middle = (left + right) / 2;
-
-            if (middle > 0 && nums[middle - 1] > nums[middle]) return nums[middle];
-
-            if (nums[middle] > nums[right]) left = middle + 1;
-            else right = middle - 1;
+        while (l < r) {
+            mid = (r + l) / 2;
+            if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
         }
 
-        return -1;
-
+        return nums[r];
     }
 }
