72904aa0e677ace3b1e4f0c6ae89a77a2bd4e4a2
Elchibek Dastanov <elchibekdastanov@gmail.com>
2026-05-17 23:12:22 -0500

    Refactor lowestCommonAncestor to use iteration

--- LeetCode_Problems/Java/medium/0235. Lowest Common Ancestor of a Binary Search Tree.java
+++ LeetCode_Problems/Java/medium/0235. Lowest Common Ancestor of a Binary Search Tree.java
@@ -10,16 +10,21 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-       if (root == null) return null;
 
-       if (root.val > p.val && root.val > q.val) {
-        return lowestCommonAncestor(root.left, p, q);
-       }
+        while (root != null) {
+            if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            }
 
-       if (root.val < p.val && root.val < q.val) {
-        return lowestCommonAncestor(root.right, p, q);
-       }
+            else if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            }
+            
+            else {
+                return root;
+            }
+        }
 
-       return root;
+        return null;
     }
 }
