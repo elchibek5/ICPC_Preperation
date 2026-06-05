a0421458380ec19106055aed44dd7b5073f5caf0
Elchibek Dastanov <elchibekdastanov@gmail.com>
2026-06-05 18:31:40 -0500

    Refactor complement calculation logic
    Refactor complement calculation to use a list for binary digits.

--- LeetCode_Problems/Java/easy/1009. Complement of Base 10 Integer.java
+++ LeetCode_Problems/Java/easy/1009. Complement of Base 10 Integer.java
@@ -4,22 +4,25 @@ class Solution {
             return 1;
         }
 
-        int ans = 0;
-        int power = 1;
+        List<Integer> list = new ArrayList<>();
 
         while (n > 0) {
-            int digit = n % 2;
+            list.add(n % 2);
+            n = n / 2;
+        }
 
+        int ans = 0;
+        int power = 0;
+        for (int i = 0; i < list.size(); i++) {
+            int digit = list.get(i);
             if (digit == 0) {
                 digit = 1;
             } else {
                 digit = 0;
             }
-
-            ans += digit * power;
-
-            power *= 2;
-            n /= 2;
+            int cast = (int) Math.pow(2, power);
+            ans += digit * cast;
+            power++;
         }
 
         return ans;
