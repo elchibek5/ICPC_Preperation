1bf23a97bf6a89b66f67329534563c9a2c5b13c1
Elchibek Dastanov <elchibekdastanov@gmail.com>
2026-06-09 18:04:12 -0500

    Simplify Hamming Distance calculation using bitCount

--- LeetCode_Problems/Java/easy/0461. Hamming Distance.java
+++ LeetCode_Problems/Java/easy/0461. Hamming Distance.java
@@ -1,13 +1,6 @@
 class Solution {
     public int hammingDistance(int x, int y) {
         int current = x ^ y;
-        int count = 0;
-
-        while (current > 0) {
-            if ((current & 1) == 1) count++;
-            current = current >> 1;  
-        }
-
-        return count;
+        return Integer.bitCount(current);
     }
 }
