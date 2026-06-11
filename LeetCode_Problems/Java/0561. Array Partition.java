33329a54a003c61b60919761c5d4ba5068bc6470
Elchibek Dastanov <elchibekdastanov@gmail.com>
2026-06-11 16:34:13 -0500

    Optimize arrayPairSum loop for better performance

--- LeetCode_Problems/Java/0561. Array Partition.java
+++ LeetCode_Problems/Java/0561. Array Partition.java
@@ -3,8 +3,8 @@ class Solution {
         Arrays.sort(nums);
         int result = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (i % 2 == 0) result += nums[i];
+        for (int i = 0; i < nums.length; i += 2) {
+            result += nums[i];
         }
 
         return result;
