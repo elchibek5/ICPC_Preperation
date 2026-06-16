5ebb89dc538bec95d74765333dc8cb658c8ecab3
Elchibek Dastanov <elchibekdastanov@gmail.com>
2026-06-16 15:08:34 -0500

    Add solution for Teemo Attacking problem

---
+++ LeetCode_Problems/Java/easy/0495. Teemo Attacking.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int time = 0;
+
+        for (int i = 0; i < timeSeries.length - 1; i++) {
+            time += Math.min(timeSeries[i + 1] - timeSeries[i], duration);
+        }
+
+        time += duration;
+        return time;
+    }
+}
